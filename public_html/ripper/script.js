78eca83527883330bb2e854fa3ff0bdefd2c3f45
Kulivszk <kulivszk@gmail.com>
2016-04-28 23:02:56 +0200

    RiPPER 0.0.4.1

--- public_html/ripper/script.js
+++ public_html/ripper/script.js
@@ -5,6 +5,8 @@ var changelog = [
 	'--- Cambiada la fuente a monospace',
 	'-- Añadido un pequeño tutorial.',
 	'-- Añadido un generador de nombres de marcas y modelos de ordenador.',
+	'-- Añadida una tienda de ordenadores donde se podrán comprar nuevos ordenadores.',
+	'--- Añadida durabilidad a los ordenadores',
 ];
 var cu = 0;
 
@@ -16,6 +18,7 @@ function resetVariables() {
 	if (!ripper.tutorial) ripper.tutorial = 1;
 	if (!ripper.value) ripper.value = 0;
 	if (!ripper.money && ripper.money != 0) ripper.money = 500;
+	if (!ripper.army) ripper.army = [];
 }
 function saveGame() {
 	localStorage.setItem('ripper', JSON.stringify(ripper));
@@ -34,7 +37,7 @@ function update(step) {
 function displayText(string) {
 	return string+'<span class="insertionPoint">_</span>';
 }
-function Computer(brand, cpu, lifespan) {
+function Computer(brand, cpu, lifespan, durability) {
 	var value = cu;
 	cu++;
 	this.brand = computerBrands[brand];
@@ -44,27 +47,59 @@ function Computer(brand, cpu, lifespan) {
 	this.model = mod[max]+'-'+val.toString(36).toUpperCase();
 	this.cpu = cpu;           //The higher cpu power, the faster you crack those pesky passwords
 	this.lifespan = lifespan; //The higher lifespan, the more your computer will last
-	this.price = Math.ceil((cpu * 100) + (lifespan * 150));
+	this.price = 50 + Math.ceil((cpu * 160) + (lifespan * 250) + (durability * 0.75));
+	this.durability = durability;
 }
-function SlowType(where, text) {
+function SlowType(where, text, callback) {
 	this.where = where;
 	this.text = text;
+	var d = doc(where).innerHTML.replace('insertionPoint', 'insertionPointInvis');
+	this.oldText = d;
 	this.index = 0;
+	this.callback = callback;
 }
-function addSlowType(where, text) {
-	slowTyping.push(new SlowType(where, text));
+function addSlowType(where, text, callback) {
+	slowTyping.push(new SlowType(where, text, callback));
 }
 function usl() {
-	for (var st in slowTyping) {
-		var styper = slowTyping[st];
-		var wh = styper.where;
-		var txt = styper.text;
-		styper.index++;
-		var ix = styper.index;
-		doc(wh).innerHTML = displayText(txt.slice(0, ix));
-		if (ix >= txt.length) {
-			slowTyping.splice(st, 1);
-		}
+	var styper = slowTyping[0];
+	if (!styper) return;
+	var wh = styper.where;
+	var txt = styper.text;
+	styper.index++;
+	var ix = styper.index;
+	var oldText = styper.oldText;
+	if (!oldText) oldText = '';
+	doc(wh).innerHTML = gameLog.join('<br>')+'<br>'+displayText(txt.slice(0, ix));
+	if (ix >= txt.length) {
+		if (styper.callback) var x = styper.callback();
+		gameLog.push(txt);
+		slowTyping.splice(0, 1);
+	}
+}
+function updateShop() {
+	var l = '<h2>Computer Shop</h2><i>Click any computer to buy it</i><br>';
+	for (var us in computers) {
+		l += '<div class="computer" onclick="buyComputer('+us+')">'+displayComputerData(computers[us])+'</div>';
+	}
+	computerShop.innerHTML = l;
+}
+function buyComputer(computerID) {
+
+}
+function displayComputerData(computer) {
+	return '<b>'+computer.brand+'</b> <i>'+computer.model+'</i><br> ($'+shortNum(computer.price)+')<br>CPU level '+computer.cpu+'<br>Lifespan '+computer.lifespan+'<br>Durability '+computer.durability;
+}
+function tutorial() {
+	if (ripper.tutorial <= 1) {
+		addSlowType('startGame', 'Welcome to RiPPER v'+gameInfo.version);
+
+		addSlowType('startGame', 'As a part of your initiation test, you should buy a new computer.');
+
+		addSlowType('startGame', 'You have $'+ripper.money+' in hand. Choose wisely.', function() {
+			$('#computerShop').fadeIn(1000);
+			updateShop();
+		});
 	}
 }
 
@@ -97,19 +132,18 @@ var models = [
 ];
 
 var computers = [
-    new Computer(0, 0.1, 0.1),
-    new Computer(1, 0.15, 0.1),
-    new Computer(2, 0.1, 0.15),
-
-    new Computer(3, 0.2, 0.2),
-    new Computer(4, 0.4, 0.2),
-
-    new Computer(0, 0.8, 0.8),
-    new Computer(1, 1.2, 0.8),
-    new Computer(2, 0.8, 1.2),
-
-    new Computer(3, 1.3, 1.3),
-    new Computer(4, 2.5, 1.2),
+    //Level 1 Computers
+    new Computer(0,   0.5,   0.5,   100), //PHACER Computers have balanced CPU power, lifespan and durability
+    new Computer(1,   1,     0.5,   100), //PH Computers have better CPU power
+    new Computer(2,   0.5,   1,     100), //Sonapanic Computers have a longer lifespan
+    new Computer(3,   0.5,   0.5,   200), //FUSHUHTZU Computers have higher durability
+    new Computer(4,   1,     1,     50),  //Lemon Computers are good in CPU power and lifespan, but have low durability
+    //Level 2 Computers
+    new Computer(0,   1,     1,    200),
+    new Computer(1,   2,     1,    200),
+    new Computer(2,   1,     2,    200),
+    new Computer(3,   1,     1,    400),
+    new Computer(4,   2,     2,    100),
 ];
 var slowTyping = [];
 var ripper = {};
@@ -118,14 +152,11 @@ var gameInfo = {
 	'version': changes(changelog).latestVersion,
 	'changelog': changes(changelog).changelog,
 }
+var gameLog = [];
 loadGame();
 resetVariables();
 update();
 var t = setInterval(saveGame, 60000);
 var ust = setInterval(function() {usl();}, 50);
 
-if (ripper.tutorial <= 1) {
-	addSlowType('startGame', 'Welcome to RiPPER v'+gameInfo.version+'<br> \
-	As a part of your initiation test, you should buy a new computer. \
-	<br>You have $'+ripper.money+' in hand. Choose wisely.');
-}
+tutorial();
